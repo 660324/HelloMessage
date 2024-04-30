c32cc265e50163abd86e4a48159a4fced2a86e33
660324 <59671452+660324@users.noreply.github.com>
2024-04-29 19:03:15 -0500

    admit_enroll.py

--- admit_enroll.py
+++ admit_enroll.py
@@ -4,3 +4,9 @@ if st.button('Say hello'):
    st.write('Hello!')
 else:
     st.write('Please click the button')
+
+uploaded_file = st.file_uploader("Upload a File Here", type=["csv"])
+if uploaded_file is not None:
+   st.write('Uploaded')
+
+
